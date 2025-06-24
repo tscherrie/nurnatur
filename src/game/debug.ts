9c2b08f58a4313f5ba4b5405a3ca26ab0f823c87
tscherrie <grenzebach@protonmail.com>
2025-06-24 17:13:19 +0300

    fix: support non-browser environments in debug util

--- src/game/debug.ts
+++ src/game/debug.ts
@@ -1,9 +1,12 @@
 // src/game/debug.ts
 
 // Debug mode can be activated by adding ?debug=true to the URL
-const urlParams = new URLSearchParams(window.location.search);
+const urlParams =
+  typeof window !== 'undefined'
+    ? new URLSearchParams(window.location.search)
+    : new URLSearchParams('');
 export const IS_DEBUG_MODE = urlParams.get('debug') === 'true';
 
-if (IS_DEBUG_MODE) {
+if (IS_DEBUG_MODE && typeof window !== 'undefined') {
   console.log('%c-- DEBUG MODE ON --', 'color: red; font-weight: bold;');
-} 
+}
